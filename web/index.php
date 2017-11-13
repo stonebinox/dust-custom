053bbfe08a17c6c1f09b390fa0670f3ae572fb80
stonebinox <anoop.santhanam@gmail.com>
2017-11-13 18:49:26 +0530

    Initial layout

--- web/index.php
+++ web/index.php
@@ -1,5 +1,4 @@
 <?php
-
 ini_set('display_errors', 1);
 require_once __DIR__.'/../vendor/autoload.php';
 $app = require __DIR__.'/../src/app.php';
@@ -12,15 +11,6 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 ));
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
-));
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-      'driver' => 'pdo_mysql',
-      'dbname' => 'heroku_d79883d9aa66a0a',
-      'user' => 'b46d8bdcf97357',
-      'password' => '8fdecc7f ',
-      'host'=> "us-cdbr-iron-east-05.cleardb.net",
-    )
 ));
 $app->register(new Silex\Provider\SessionServiceProvider, array(
     'session.storage.save_path' => dirname(__DIR__) . '/tmp/sessions'
---
+++ web/js/custom.js
@@ -0,0 +1,4 @@
+var app=angular.module("custom",[]);
+app.controller("mvp",function($scope,$compile,$http){
+    
+});
